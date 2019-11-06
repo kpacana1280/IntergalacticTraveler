157f6cdeb44fd3c605e332510f3623c8d5b032c2
Kamil Pacana <kpacana1280@stu.d214.org>
2019-11-06 11:21:06 -0600

    Added Comment

---
+++ IntergalacticTraveler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IntergalacticTraveler
+//
+//  Created by Kamil Pacana on 11/5/19.
+//  Copyright © 2019 John Hersey High School. All rights reserved.
+//this is cool
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
